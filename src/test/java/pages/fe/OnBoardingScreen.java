cf8f4ed2663c3d1735efae8690a0b167ec9a4991
Ahmed Madyan <ahmed.madyan@vodafone.com>
2023-09-17 20:25:11 +0300

    Update OnBoardingScreen.java

--- src/test/java/pages/fe/OnBoardingScreen.java
+++ src/test/java/pages/fe/OnBoardingScreen.java
@@ -4,24 +4,26 @@ import assertions.Assertions;
 import elements.Elements;
 import elements.gesture_actions.GestureDirection;
 import io.appium.java_client.AppiumBy;
+import org.json.simple.JSONObject;
 import org.openqa.selenium.By;
+import readers.json_reader.JSONDataManager;
 
 public class OnBoardingScreen {
-    private static final By onBoarding_Card_title_1 = AppiumBy.accessibilityId("onBoarding_Card_title_0");
-    private static final By onBoarding_Card_description_1 = AppiumBy.accessibilityId("onBoarding_Card_description_0");
-    private static final By onBoarding_Card_title_2 = AppiumBy.accessibilityId("onBoarding_Card_title_1");
-    private static final By onBoarding_Card_description_2 = AppiumBy.accessibilityId("onBoarding_Card_description_1");
-    private static final By onBoarding_Card_title_3 = AppiumBy.accessibilityId("onBoarding_Card_title_2");
-    private static final By onBoarding_Card_description_3 = AppiumBy.accessibilityId("onBoarding_Card_description_2");
-    private static final By onBoarding_Card_title_4 = AppiumBy.accessibilityId("onBoarding_Card_title_3");
-    private static final By onBoarding_Card_description_4 = AppiumBy.accessibilityId("onBoarding_Card_description_3");
-    private static final By onBoarding_Button_Component_4 = AppiumBy.accessibilityId("onBoarding_Button_Component");
-    private static final By onBoarding_Button_Text = AppiumBy.accessibilityId("Button_text");
-    private static final By pagination_Dot_1 = AppiumBy.accessibilityId("Pagination_Dot_0");
-    private static final By pagination_Dot_2 = AppiumBy.accessibilityId("Pagination_Dot_1");
-    private static final By pagination_Dot_3 = AppiumBy.accessibilityId("Pagination_Dot_2");
-    private static final By pagination_Dot_4 = AppiumBy.accessibilityId("Pagination_Dot_3");
-
+    private static final By ON_BOARDING_CARD_TITLE_1 = AppiumBy.accessibilityId("onBoarding_Card_title_0");
+    private static final By ON_BOARDING_CARD_DESCRIPTION_1 = AppiumBy.accessibilityId("onBoarding_Card_description_0");
+    private static final By ON_BOARDING_CARD_TITLE_2 = AppiumBy.accessibilityId("onBoarding_Card_title_1");
+    private static final By ON_BOARDING_CARD_DESCRIPTION_2 = AppiumBy.accessibilityId("onBoarding_Card_description_1");
+    private static final By ON_BOARDING_CARD_TITLE_3 = AppiumBy.accessibilityId("onBoarding_Card_title_2");
+    private static final By ON_BOARDING_CARD_DESCRIPTION_3 = AppiumBy.accessibilityId("onBoarding_Card_description_2");
+    private static final By ON_BOARDING_CARD_TITLE_4 = AppiumBy.accessibilityId("onBoarding_Card_title_3");
+    private static final By ON_BOARDING_CARD_DESCRIPTION_4 = AppiumBy.accessibilityId("onBoarding_Card_description_3");
+    private static final By ON_BOARDING_BUTTON_COMPONENT_4 = AppiumBy.accessibilityId("onBoarding_Button_Component");
+    private static final By ON_BOARDING_BUTTON_TEXT = AppiumBy.accessibilityId("Button_text");
+    private static final By PAGINATION_DOT_1 = AppiumBy.accessibilityId("Pagination_Dot_0");
+    private static final By PAGINATION_DOT_2 = AppiumBy.accessibilityId("Pagination_Dot_1");
+    private static final By PAGINATION_DOT_3 = AppiumBy.accessibilityId("Pagination_Dot_2");
+    private static final By PAGINATION_DOT_4 = AppiumBy.accessibilityId("Pagination_Dot_3");
+    private static final String TEST_DATA_FILE_PATH = ("src/test/resources/test_data/fe/expected/OnBoardingScreens.json");
     public static void swipeOnBoardingCards() {
         swipeOnBoardingCard_1();
         swipeOnBoardingCard_2();
@@ -30,55 +32,56 @@ public class OnBoardingScreen {
     }
 
     public static void swipeOnBoardingCard_1() {
-        Elements.gestureActions().swipe(onBoarding_Card_title_1, GestureDirection.LEFT);
+        Elements.gestureActions().swipe(ON_BOARDING_CARD_TITLE_1, GestureDirection.LEFT);
     }
 
     public static void swipeOnBoardingCard_2() {
-        Elements.gestureActions().swipe(onBoarding_Card_title_2, GestureDirection.LEFT);
+        Elements.gestureActions().swipe(ON_BOARDING_CARD_TITLE_2, GestureDirection.LEFT);
     }
 
     public static void swipeOnBoardingCard_3() {
-        Elements.gestureActions().swipe(onBoarding_Card_title_3, GestureDirection.LEFT);
+        Elements.gestureActions().swipe(ON_BOARDING_CARD_TITLE_3, GestureDirection.LEFT);
     }
 
     public static void clickOnBoardingButton() {
-        Elements.touchActions().tap(onBoarding_Button_Component_4);
+        Elements.elementActions().click(ON_BOARDING_BUTTON_COMPONENT_4);
     }
 
     public static void assertThatUserNavigatedToOnBoardingScreen() {
-        Assertions.hardAssert().elementDisplayed(onBoarding_Card_description_1);
+        Assertions.hardAssert().elementDisplayed(ON_BOARDING_CARD_DESCRIPTION_1);
     }
 
     public static void assertThatAllElementsInTheFirstOnBoardingScreenExists() {
-        Assertions.hardAssert().elementDisplayed(onBoarding_Card_description_1);
-        Assertions.hardAssert().elementDisplayed(onBoarding_Card_title_1);
-        Assertions.hardAssert().elementDisplayed(pagination_Dot_1);
-        Assertions.hardAssert().elementDisplayed(pagination_Dot_2);
-        Assertions.hardAssert().elementDisplayed(pagination_Dot_3);
-        Assertions.hardAssert().elementDisplayed(pagination_Dot_4);
+        Assertions.hardAssert().elementDisplayed(ON_BOARDING_CARD_DESCRIPTION_1);
+        Assertions.hardAssert().elementDisplayed(ON_BOARDING_CARD_TITLE_1);
+        Assertions.hardAssert().elementDisplayed(PAGINATION_DOT_1);
+        Assertions.hardAssert().elementDisplayed(PAGINATION_DOT_2);
+        Assertions.hardAssert().elementDisplayed(PAGINATION_DOT_3);
+        Assertions.hardAssert().elementDisplayed(PAGINATION_DOT_4);
     }
 
-    public static void assertOnBoardingTitleAndDescriptionTextForCardOne(String expectedValue_Title, String expectedValue_Description) {
-        Assertions.hardAssert().elementTextToBe(onBoarding_Card_title_1, expectedValue_Title);
-        Assertions.hardAssert().elementTextToBe(onBoarding_Card_description_1, expectedValue_Description);
+    public static void assertOnBoardingTitleAndDescriptionTextForCardOne() {
+        Assertions.hardAssert().elementTextToBe(ON_BOARDING_CARD_TITLE_1, JSONDataManager.getJSONData(TEST_DATA_FILE_PATH, "onBoarding_Card_Title_1", JSONDataManager.Types.STRING).toString());
+        Assertions.hardAssert().elementTextToBe(ON_BOARDING_CARD_DESCRIPTION_1, JSONDataManager.getJSONData(TEST_DATA_FILE_PATH, "onBoarding_Card_Description_1", JSONDataManager.Types.STRING).toString());
     }
 
-    public static void assertOnBoardingTitleAndDescriptionTextForCardTwo(String expectedValue_Title, String expectedValue_Description) {
-        Assertions.hardAssert().elementTextToBe(onBoarding_Card_title_2, expectedValue_Title);
-        Assertions.hardAssert().elementTextToBe(onBoarding_Card_description_2, expectedValue_Description);
+    public static void assertOnBoardingTitleAndDescriptionTextForCardTwo() {
+        Assertions.hardAssert().elementTextToBe(ON_BOARDING_CARD_TITLE_2, JSONDataManager.getJSONData(TEST_DATA_FILE_PATH, "onBoarding_Card_Title_2", JSONDataManager.Types.STRING).toString());
+        Assertions.hardAssert().elementTextToBe(ON_BOARDING_CARD_DESCRIPTION_2, JSONDataManager.getJSONData(TEST_DATA_FILE_PATH, "onBoarding_Card_Description_2", JSONDataManager.Types.STRING).toString());
     }
 
-    public static void assertOnBoardingTitleAndDescriptionTextForCardThree(String expectedValue_Title, String expectedValue_Description) {
-        Assertions.hardAssert().elementTextToBe(onBoarding_Card_title_3, expectedValue_Title);
-        Assertions.hardAssert().elementTextToBe(onBoarding_Card_description_3, expectedValue_Description);
+    public static void assertOnBoardingTitleAndDescriptionTextForCardThree() {
+        Assertions.hardAssert().elementTextToBe(ON_BOARDING_CARD_TITLE_3, JSONDataManager.getJSONData(TEST_DATA_FILE_PATH, "onBoarding_Card_Title_3", JSONDataManager.Types.STRING).toString());
+        Assertions.hardAssert().elementTextToBe(ON_BOARDING_CARD_DESCRIPTION_3, JSONDataManager.getJSONData(TEST_DATA_FILE_PATH, "onBoarding_Card_Description_3", JSONDataManager.Types.STRING).toString());
     }
 
-    public static void assertOnBoardingTitleAndDescriptionTextForCardFour(String expectedValue_Title, String expectedValue_Description) {
-        Assertions.hardAssert().elementTextToBe(onBoarding_Card_title_4, expectedValue_Title);
-        Assertions.hardAssert().elementTextToBe(onBoarding_Card_description_4, expectedValue_Description);
+    public static void assertOnBoardingTitleAndDescriptionTextForCardFour() {
+        Assertions.hardAssert().elementTextToBe(ON_BOARDING_CARD_TITLE_4, JSONDataManager.getJSONData(TEST_DATA_FILE_PATH, "onBoarding_Card_Title_4", JSONDataManager.Types.STRING).toString());
+        Assertions.hardAssert().elementTextToBe(ON_BOARDING_CARD_DESCRIPTION_4, JSONDataManager.getJSONData(TEST_DATA_FILE_PATH, "onBoarding_Card_Description_4", JSONDataManager.Types.STRING).toString());
     }
 
-    public static void assertOnBoardingButtonComponentTextForCardFour(String expectedValue_Button) {
-        Assertions.hardAssert().elementTextToBe(onBoarding_Button_Text, expectedValue_Button);
+    public static void assertOnBoardingButtonComponentTextForCardFour() {
+
+        Assertions.hardAssert().elementTextToBe(ON_BOARDING_BUTTON_TEXT, JSONDataManager.getJSONData(TEST_DATA_FILE_PATH, "onBoarding_Card_Button_Component", JSONDataManager.Types.STRING).toString());
     }
 }
