799848ce41a10a1e852f5a7d682bdb3d5dbe1870
Ahmed Madyan <ahmed.madyan@vodafone.com>
2023-09-18 00:06:02 +0300

    Update CLI.java

--- src/main/java/cli/CLI.java
+++ src/main/java/cli/CLI.java
@@ -1,32 +1,21 @@
 package cli;
 
-import exceptions.Exceptions;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class CLI {
-    public static void executeCommandLine(String command)  {
+    public static void executeCommandLine(String command) throws IOException {
         ProcessBuilder builder = new ProcessBuilder(
                 "cmd.exe", "/c", command);
         builder.redirectErrorStream(true);
-        Process p = null;
-        try {
-            p = builder.start();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        Process p = builder.start();
         BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
-        String line = null;
-        while (true) {
-            try {
-                if (!((line = r.readLine()) != null)) break;
-            } catch (IOException e) {
-                Exceptions.handle(e);
-            }
+        String line;
+        while ((line = r.readLine()) != null) {
             System.out.println(line);
             break;
         }
     }
 }
+
