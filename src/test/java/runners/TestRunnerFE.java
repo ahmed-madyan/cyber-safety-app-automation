29fbcf7895c89a7c6fc2c95324de131cf4dfd772
Ahmed Madyan <ahmed.madyan@vodafone.com>
2023-09-17 15:51:19 +0300

    Update TestRunnerFE.java

--- src/test/java/runners/TestRunnerFE.java
+++ src/test/java/runners/TestRunnerFE.java
@@ -1,23 +1,16 @@
 package runners;
 
-import driver.BrowserStackBuildIdentifier;
 import driver.DriverInitializer;
 import io.cucumber.testng.CucumberOptions;
-import org.testng.annotations.BeforeClass;
 
 @CucumberOptions
         (
                 publish = true,
                 features = {"src/test/java/features"},
                 glue = {"steps"},
-                tags = ("@OnBoardingScreen"),
+                tags = ("@Regression_FE"),
                 plugin = {"pretty",
                         "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
 
 public class TestRunnerFE extends DriverInitializer {
-//    @BeforeClass(alwaysRun = true)
-//    public void generateBuildIdentifier() {
-//        BrowserStackBuildIdentifier.generateBuildNumber();
-//        BrowserStackBuildIdentifier.generateBuildIdentifierDateTime();
-//    }
 }
