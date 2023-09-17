3785d926d30f797229954076c75d712d55a2d2d5
Ahmed Madyan <ahmed.madyan@vodafone.com>
2023-09-17 23:39:58 +0300

    Create AllureReport.java

---
+++ src/main/java/reports/AllureReport.java
@@ -0,0 +1,14 @@
+package reports;
+
+import cli.CLI;
+
+public class AllureReport {
+    public void deleteOutDatedAllureReport() {
+        CLI.executeCommandLine("cd allure-results");
+        CLI.executeCommandLine("DEL /S /Q *.json");
+    }
+
+    public void generateAllureReport() {
+        CLI.executeCommandLine("allure serve allure-results");
+    }
+}
