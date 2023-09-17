79693bb152bc3089b989686cc956a06705220b55
Ahmed Madyan <ahmed.madyan@vodafone.com>
2023-09-17 20:24:58 +0300

    Update TopicsScreen.java

--- src/test/java/pages/fe/TopicsScreen.java
+++ src/test/java/pages/fe/TopicsScreen.java
@@ -7,73 +7,74 @@ import org.openqa.selenium.By;
 import readers.json_reader.JSONDataManager;
 
 public class TopicsScreen {
-    private static final By topicsScreen_SkipButton = AppiumBy.accessibilityId("TopicsScreen_Skip_text");
-    private static final By topicsScreen_title_text = AppiumBy.accessibilityId("TopicsScreen_title_text");
-    private static final By topicsScreen_subTitle_text = AppiumBy.accessibilityId("TopicsScreen_subTitle_text");
-    private static final By topicsScreen_Socials = AppiumBy.accessibilityId("Topic_Socials");
-    private static final By topicsScreen_DealingWithDrama = AppiumBy.accessibilityId("Topic_Dealing with Drama");
-    private static final By topicsScreen_StayingZen = AppiumBy.accessibilityId("Topic_Staying Zen");
-    private static final By topicsScreen_BeatBulling = AppiumBy.accessibilityId("Topic_Beat Bulling");
-    private static final By topicsScreen_ShieldUp = AppiumBy.accessibilityId("Topic_Shield Up");
-    private static final By topicsScreen_GoButton = AppiumBy.accessibilityId("Topic_Shield Up");
-    private static final String topicsScreen_TestData_FilePath = ("src/test/resources/test_data/fe/expected/TopicsScreens.json");
+    private static final By TOPICS_SCREEN_SKIP_BUTTON = AppiumBy.accessibilityId("TopicsScreen_Skip_text");
+    private static final By TOPICS_SCREEN_TITLE_TEXT = AppiumBy.accessibilityId("TopicsScreen_title_text");
+    private static final By TOPICS_SCREEN_SUB_TITLE_TEXT = AppiumBy.accessibilityId("TopicsScreen_subTitle_text");
+    private static final By TOPICS_SCREEN_SOCIALS = AppiumBy.accessibilityId("Topic_Socials");
+    private static final By TOPICS_SCREEN_DEALING_WITH_DRAMA = AppiumBy.accessibilityId("Topic_Dealing with Drama");
+    private static final By TOPICS_SCREEN_STAYING_ZEN = AppiumBy.accessibilityId("Topic_Staying Zen");
+    private static final By TOPICS_SCREEN_BEAT_BULLING = AppiumBy.accessibilityId("Topic_Beat Bulling");
+    private static final By TOPICS_SCREEN_SHIELD_UP = AppiumBy.accessibilityId("Topic_Shield Up");
+    private static final By TOPICS_SCREEN_GO_BUTTON = AppiumBy.accessibilityId("TopicsScreen_Button");
+    private static final By TOPICS_SCREEN_GO_BUTTON_TEXT = AppiumBy.accessibilityId("Button_text");
+    private static final String TEST_DATA_FILE_PATH = ("src/test/resources/test_data/fe/expected/TopicsScreens.json");
 
     public static void assertThatUserRedirectedToTheTopicsScreen() {
-        Assertions.hardAssert().elementDisplayed(topicsScreen_title_text);
+        Assertions.hardAssert().elementDisplayed(TOPICS_SCREEN_TITLE_TEXT);
     }
 
     public static void clickSkip() {
-        Elements.elementActions().click(topicsScreen_SkipButton);
+        Elements.elementActions().click(TOPICS_SCREEN_SKIP_BUTTON);
     }
 
     public static void clickGo() {
-        Elements.elementActions().click(topicsScreen_GoButton);
+        Elements.elementActions().click(TOPICS_SCREEN_GO_BUTTON);
     }
 
     public static void assertSkipButtonText(String expectedText) {
-        Assertions.hardAssert().elementDisplayed(topicsScreen_SkipButton);
-        Assertions.hardAssert().elementAttributeToBe(topicsScreen_SkipButton, "text", expectedText);
+        Assertions.hardAssert().elementDisplayed(TOPICS_SCREEN_SKIP_BUTTON);
+        Assertions.hardAssert().elementAttributeToBe(TOPICS_SCREEN_SKIP_BUTTON, "text", expectedText);
     }
 
     public static void assertGoButtonText(String expectedText) {
-        Assertions.hardAssert().elementDisplayed(topicsScreen_GoButton);
-        Assertions.hardAssert().elementAttributeToBe(topicsScreen_GoButton, "text", expectedText);
+        Assertions.hardAssert().elementDisplayed(TOPICS_SCREEN_GO_BUTTON);
+        Assertions.hardAssert().elementAttributeToBe(TOPICS_SCREEN_GO_BUTTON_TEXT, "text", expectedText);
     }
 
     public static void assertTopicsScreenContent() {
-        assertExploreHeaderTitleText(JSONDataManager.getJSONData(topicsScreen_TestData_FilePath, "explore_Header_Title", JSONDataManager.Types.STRING).toString());
-        assertExploreHeaderSubTitleText(JSONDataManager.getJSONData(topicsScreen_TestData_FilePath, "explore_Header_Sub_Title", JSONDataManager.Types.STRING).toString());
-        assertSkipButtonText(JSONDataManager.getJSONData(topicsScreen_TestData_FilePath, "skip_Button", JSONDataManager.Types.STRING).toString());
-        assertGoButtonText(JSONDataManager.getJSONData(topicsScreen_TestData_FilePath, "go_Button", JSONDataManager.Types.STRING).toString());
+        assertExploreHeaderTitleText(JSONDataManager.getJSONData(TEST_DATA_FILE_PATH, "explore_Header_Title", JSONDataManager.Types.STRING).toString());
+        assertExploreHeaderSubTitleText(JSONDataManager.getJSONData(TEST_DATA_FILE_PATH, "explore_Header_Sub_Title", JSONDataManager.Types.STRING).toString());
+        assertSkipButtonText(JSONDataManager.getJSONData(TEST_DATA_FILE_PATH, "skip_Button", JSONDataManager.Types.STRING).toString());
+        assertGoButtonText(JSONDataManager.getJSONData(TEST_DATA_FILE_PATH, "go_Button", JSONDataManager.Types.STRING).toString());
     }
 
     public static void assertExploreHeaderTitleText(String expectedText) {
-        Assertions.hardAssert().elementDisplayed(topicsScreen_title_text);
-        Assertions.hardAssert().elementAttributeToBe(topicsScreen_title_text, "text", expectedText);
+        Assertions.hardAssert().elementDisplayed(TOPICS_SCREEN_TITLE_TEXT);
+        Assertions.hardAssert().elementAttributeToBe(TOPICS_SCREEN_TITLE_TEXT, "text", expectedText);
     }
 
     public static void assertExploreHeaderSubTitleText(String expectedText) {
-        Assertions.hardAssert().elementDisplayed(topicsScreen_subTitle_text);
-        Assertions.hardAssert().elementAttributeToBe(topicsScreen_subTitle_text, "text", expectedText);
+        Assertions.hardAssert().elementDisplayed(TOPICS_SCREEN_SUB_TITLE_TEXT);
+        Assertions.hardAssert().elementAttributeToBe(TOPICS_SCREEN_SUB_TITLE_TEXT, "text", expectedText);
     }
 
     public static void selectTopicSocials() {
-        Elements.elementActions().click(topicsScreen_Socials);
+        Elements.elementActions().click(TOPICS_SCREEN_SOCIALS);
     }
 
     public static void selectTopicDealingWithDrama() {
-        Elements.elementActions().click(topicsScreen_DealingWithDrama);
+        Elements.elementActions().click(TOPICS_SCREEN_DEALING_WITH_DRAMA);
     }
 
     public static void selectTopicStayingZen() {
-        Elements.elementActions().click(topicsScreen_StayingZen);
+        Elements.elementActions().click(TOPICS_SCREEN_STAYING_ZEN);
     }
 
     public static void selectTopicBeatBulling() {
-        Elements.elementActions().click(topicsScreen_BeatBulling);
+        Elements.elementActions().click(TOPICS_SCREEN_BEAT_BULLING);
     }
 
     public static void selectTopicShieldUp() {
-        Elements.elementActions().click(topicsScreen_ShieldUp);
+        Elements.elementActions().click(TOPICS_SCREEN_SHIELD_UP);
     }
 }
