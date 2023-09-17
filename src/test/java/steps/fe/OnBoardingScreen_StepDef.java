70b4796b24ba47387209cfb3dc48e3c76602d866
Ahmed Madyan <ahmed.madyan@vodafone.com>
2023-09-17 20:25:05 +0300

    Update OnBoardingScreen_StepDef.java

--- src/test/java/steps/fe/OnBoardingScreen_StepDef.java
+++ src/test/java/steps/fe/OnBoardingScreen_StepDef.java
@@ -3,13 +3,10 @@ package steps.fe;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.json.simple.JSONObject;
 import pages.fe.OnBoardingScreen;
-import readers.json_reader.JSONDataManager;
 
 public class OnBoardingScreen_StepDef {
-    private static final String test_Data_File_Path = ("src/test/resources/test_data/fe/expected/OnBoardingScreens.json");
-    private static final JSONObject test_Data = JSONDataManager.parseJSON(test_Data_File_Path);
+
 
     @Given("User navigates to on boarding screen")
     public void userNavigatesToOnBoardingScreen() {
@@ -23,22 +20,22 @@ public class OnBoardingScreen_StepDef {
 
     @Then("On boarding title and description text for card one should be displayed")
     public void onBoardingTitleAndDescriptionTextForCardOneShouldBeDisplayed() {
-        OnBoardingScreen.assertOnBoardingTitleAndDescriptionTextForCardOne(test_Data.get("onBoarding_Card_Title_1").toString(), test_Data.get("onBoarding_Card_Description_1").toString());
+        OnBoardingScreen.assertOnBoardingTitleAndDescriptionTextForCardOne();
     }
 
     @Then("On boarding title and description text for card two should be displayed")
     public void onBoardingTitleAndDescriptionTextForCardTwoShouldBeDisplayed() {
-        OnBoardingScreen.assertOnBoardingTitleAndDescriptionTextForCardTwo(test_Data.get("onBoarding_Card_Title_2").toString(), test_Data.get("onBoarding_Card_Description_2").toString());
+        OnBoardingScreen.assertOnBoardingTitleAndDescriptionTextForCardTwo();
     }
 
     @Then("On boarding title and description text for card three should be displayed")
     public void onBoardingTitleAndDescriptionTextForCardThreeShouldBeDisplayed() {
-        OnBoardingScreen.assertOnBoardingTitleAndDescriptionTextForCardThree(test_Data.get("onBoarding_Card_Title_3").toString(), test_Data.get("onBoarding_Card_Description_3").toString());
+        OnBoardingScreen.assertOnBoardingTitleAndDescriptionTextForCardThree();
     }
 
     @Then("On boarding title and description text for card four should be displayed")
     public void onBoardingTitleAndDescriptionTextForCardFourShouldBeDisplayed() {
-//        OnBoardingScreen.assertOnBoardingTitleAndDescriptionTextForCardFour(test_Data.get("onBoarding_Card_Title_4").toString(), test_Data.get("onBoarding_Card_Description_4").toString());
+        OnBoardingScreen.assertOnBoardingTitleAndDescriptionTextForCardFour();
     }
 
     @When("Swipe to card two")
@@ -63,6 +60,6 @@ public class OnBoardingScreen_StepDef {
 
     @Then("On boarding button for card four should be displayed")
     public void onBoardingButtonForCardFourShouldBeDisplayed() {
-        OnBoardingScreen.assertOnBoardingButtonComponentTextForCardFour(test_Data.get("onBoarding_Card_Button_Component").toString());
+        OnBoardingScreen.assertOnBoardingButtonComponentTextForCardFour();
     }
 }
