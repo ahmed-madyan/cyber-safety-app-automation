9c2dd199979fd4e51625dbae376281237362253d
Ahmed Madyan <ahmed.madyan@vodafone.com>
2023-09-18 00:06:06 +0300

    Update AllureReport.java

--- src/main/java/reports/AllureReport.java
+++ src/main/java/reports/AllureReport.java
@@ -2,13 +2,23 @@ package reports;
 
 import cli.CLI;
 
+import java.io.IOException;
+
 public class AllureReport {
-    public void deleteOutDatedAllureReport() {
+    public static void main(String[] args) throws IOException {
+        deleteOutDatedAllureReport();
+        installNPMAllureWrapper();
+        generateAllureReport();
+    }
+    public static void deleteOutDatedAllureReport() throws IOException {
         CLI.executeCommandLine("cd allure-results");
         CLI.executeCommandLine("DEL /S /Q *.json");
     }
 
-    public void generateAllureReport() {
+    public static void installNPMAllureWrapper() throws IOException {
+        CLI.executeCommandLine("npm install -g allure-commandline --save-dev");
+    }
+    public static void generateAllureReport() throws IOException {
         CLI.executeCommandLine("allure serve allure-results");
     }
 }
