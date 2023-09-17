654a4fbd8cfca58231a5843a6259b672c2c5ef61
Ahmed Madyan <ahmed.madyan@vodafone.com>
2023-09-17 23:08:23 +0300

    Update Exceptions.java

--- src/main/java/exceptions/Exceptions.java
+++ src/main/java/exceptions/Exceptions.java
@@ -4,6 +4,5 @@ public class Exceptions {
     public static void handle(Exception exception) {
         System.out.println(exception.getMessage() + "\n" + exception.getCause() + "\n");
         exception.printStackTrace();
-        throw new RuntimeException(exception);
     }
 }
