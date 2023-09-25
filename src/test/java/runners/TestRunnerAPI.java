defadd2ff8e87c187e35eda8c46a14f6fda888fe
Ahmed Madyan <ahmed.madyan@vodafone.com>
2023-09-25 21:46:43 +0300

    Update TestRunnerAPI.java

--- src/test/java/runners/TestRunnerAPI.java
+++ src/test/java/runners/TestRunnerAPI.java
@@ -1,39 +1,17 @@
 package runners;
 
-import api.base_uris.BaseURI;
-import context.Context;
-import context.ScenarioContext;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import log4j_logger.Log4JLogger;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
 
 @CucumberOptions
         (
                 publish = true,
                 features = {"src/test/java/features"},
                 glue = {"steps"},
-                tags = ("@Get_Profile"),
+                tags = ("@Regression_API"),
                 plugin = {"pretty",
                         "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
         )
 
 public class TestRunnerAPI extends AbstractTestNGCucumberTests {
-    @BeforeClass
-    @Parameters("testEnvironment")
-    public void initializeConfigurations(@Optional("SIT") String testEnvironment) {
-        switch (testEnvironment) {
-            case "ST" -> {
-                ScenarioContext.setContext(Context.TARGET_ENVIRONMENT, testEnvironment);
-                ScenarioContext.setContext(Context.TARGET_BASE_URI, BaseURI.ST);
-            }
-            case "SIT" -> {
-                ScenarioContext.setContext(Context.TARGET_ENVIRONMENT, testEnvironment);
-                ScenarioContext.setContext(Context.TARGET_BASE_URI, BaseURI.SIT);
-            }
-            default -> Log4JLogger.logWARN("Kindly select valid test Environment [<!-- ST or SIT -->].");
-        }
-    }
 }
