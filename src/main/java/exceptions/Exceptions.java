1e0dc22fee70b010c283132b8e2d42d2f3fc7317
Ahmed Madyan <ahmed.madyan@vodafone.com>
2023-09-26 16:17:10 +0300

    Update Exceptions.java

--- src/main/java/exceptions/Exceptions.java
+++ src/main/java/exceptions/Exceptions.java
@@ -6,8 +6,8 @@ import java.util.Arrays;
 
 public class Exceptions {
     public static void handle(Class<?> c, Exception exception) {
-        Log4JLogger.logERROR(c, "Error message: " + exception.getMessage());
-        Log4JLogger.logERROR(c, "Error Cause: " + exception.getCause());
+//        Log4JLogger.logERROR(c, "Error message: " + exception.getMessage());
+//        Log4JLogger.logERROR(c, "Error Cause: " + exception.getCause());
         Log4JLogger.logERROR(c, "StackTrace: " + Arrays.toString(exception.getStackTrace()) + "Error Cause: " + exception.getCause());
     }
 }
