3556f329121369d11f2421aa80bc349f4aac4d62
Ahmed Madyan <ahmed.madyan01@gmail.com>
2023-10-24 15:18:34 +0300

    Update RequestSender.java

--- src/main/java/api/driver/RequestSender.java
+++ src/main/java/api/driver/RequestSender.java
@@ -4,8 +4,7 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 abstract class RequestSender {
-    abstract Response send(RequestSpecification requestSpecification);
+    protected abstract Response send(RequestSpecification requestSpecification);
 
-    abstract Response send(RequestSpecification requestSpecification, Integer httpStatus);
-
+    protected abstract Response send(RequestSpecification requestSpecification, Integer httpStatus);
 }
