03130ad339db236cedbd4790b2c3083b9d978b23
ahmed-madyan <ahmed.madyan01@gmail.com>
2023-11-06 14:13:06 +0200

    Update Jenkinsfile
    Signed-off-by: ahmed-madyan <ahmed.madyan01@gmail.com>

---
+++ Jemkinsfile.groovy
@@ -0,0 +1,42 @@
+pipeline {
+    agent any
+    tools {
+        // Install the Maven version configured as "M3" and add it to the path.
+        maven "MAVEN_HOME"
+    }
+    options {
+        skipStagesAfterUnstable()
+    }
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Clean and compile..'
+                sh 'mvn clean compile'
+            }
+        }
+        stage('API Testing') {
+            steps {
+                echo 'API Testing..'
+                sh 'mvn test -DsuiteXmlFile=testng-api.xml'
+            }
+        }
+        stage('Android Testing') {
+            steps {
+                echo 'Android Testing..'
+                sudo 'mvn test -DsuiteXmlFile=testng-android.xml'
+            }
+        }
+        stage('Extent Report') {
+            steps {
+                echo '$cd'
+                publishHTML(target: [allowMissing         : false,
+                                     alwaysLinkToLastBuild: true,
+                                     keepAll              : true,
+                                     reportDir            : 'SparkReport',
+                                     reportFiles          : 'ExtentReport.html',
+                                     reportName           : 'My Reports',
+                                     reportTitles         : 'The Report'])
+            }
+        }
+    }
+}
